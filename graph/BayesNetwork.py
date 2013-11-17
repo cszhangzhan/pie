f2a4f34687e15b3f94bf3a62c29c916f51cbaeed
cszz <cszhangzhan@gmail.com>
2013-11-17 15:12:39 -0500

    Class Data added.

--- graph/BayesNetwork.py
+++ graph/BayesNetwork.py
@@ -46,9 +46,28 @@ class Node:
 		self.pi = []
 
 class Data:
-	def __init__(self, datafilename):
-		return
-
+	def __init__(self):
+		f = open('history.data')
+	    lines = f.readlines()
+	    self.history_matrix = []
+	    for line in lines:
+	        line = line.strip('\n')
+	        feature_list = [int(feature) for feature in line.split(' ')]
+	        self.history_matrix.append(feature_list)
+	
+	def count_feature(self, filter_list):
+	    counter = 0
+	    for feature_list in self.history_matrix:
+	        match = True
+	        for t in filter_list:
+	            index = t[0]
+	            value = t[1]
+	            if feature_list[index] != value:
+	                match = False
+	                break
+	        if match:
+	            counter += 1
+	    return counter
 
 if __name__ == '__main__':
 	net = Network('NetworkData')
